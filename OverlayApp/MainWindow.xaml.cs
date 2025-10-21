390047e34c0110f77e732d3a3333dbbd737aa57d
Евгений Данилов <ewgendanidin@gmail.com>
2025-10-21 09:01:00 +0300

    123

--- OverlayApp/MainWindow.xaml.cs
+++ OverlayApp/MainWindow.xaml.cs
@@ -21,6 +21,22 @@ namespace OverlayApp
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
+        }
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var hwnd = new WindowInteropHelper(this).Handle;
+
+            // Устанавливаем стили окна
+            int exStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            exStyle |= WS_EX_TOPMOST;
+            SetWindowLong(hwnd, GWL_EXSTYLE, exStyle);
+
+            // Настраиваем прозрачность (255 = непрозрачно, 0 = полностью прозрачно)
+            //SetLayeredWindowAttributes(hwnd, 0, 255, LWA_ALPHA);
+            SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0,
+                    SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
+
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -59,9 +75,29 @@ namespace OverlayApp
         }
 
         // === WinAPI ===
+        const int GWL_EXSTYLE = -20;
+        const int WS_EX_TOPMOST = 0x00000008;
+        const uint SWP_NOMOVE = 0x0002;
+        const uint SWP_NOSIZE = 0x0001;
+        const uint SWP_NOACTIVATE = 0x0010;
         private const int WM_SYSCOMMAND = 0x112;
         private const int TPM_LEFTALIGN = 0x0000;
         private const int TPM_RETURNCMD = 0x0100;
+        private const int WS_EX_LAYERED = 0x00080000;
+        private const int WS_EX_TRANSPARENT = 0x00000020;
+        private const int WS_EX_TOOLWINDOW = 0x00000080;
+        private const int LWA_ALPHA = 0x2;
+        static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        [DllImport("user32.dll")]
+        static extern int GetWindowLong(IntPtr hwnd, int index);
+
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool SetWindowPos(IntPtr hwnd, IntPtr hwndInsertAfter,
+            int x, int y, int cx, int cy, uint uFlags);
+        [DllImport("user32.dll")]
+        static extern bool SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
 
         [DllImport("user32.dll")]
         private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
